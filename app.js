bd032464b186aeb33c0b3a630c4318c802eb3f82
Jesreal Arcillas <jesrealarcillas@gmail.com>
2019-02-19 17:47:48 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+const Snoowrap = require("snoowrap");
+const Snoostorm = require("snoostorm");
+
+// Build Snoowrap and Snoostorm clients
+
+const r = new Snoowrap({
+  userAgent: "reddit-bot-example-node",
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  username: process.env.REDDIT_USER,
+  password: process.env.REDDIT_PASS
+});
+
+const client = new Snoostorm(r);
+
+const streamOpts = {
+  subreddit: "testingground4bots",
+  results: 25
+};
+
+const comments = client.CommentStream(streamOpts);
+
+comments.on("comment", comment => {
+  if (comment.body === ":(") {
+    comment.reply(":)");
+  }
+});
